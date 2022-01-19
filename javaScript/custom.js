e4f33c9d0a490e396d0c4b35a46b2d28957cf212
Sunita <sunee.karma@gmail.com>
2022-01-19 08:11:29 -0800

    All HTML CSS and jQuery

---
+++ javaScript/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+            {
+                $("div").click(function()
+                {
+                    alert("simple alert message from jquery-welcome!");
+                });
+            });
